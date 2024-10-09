09f8f39ec0c68bfcb865dba8b3e3f698f77fb160
Jhean Antunes Lopes <brizadaojhean@gmail.com>
2024-10-09 12:07:10 -0300

    style: add responsividade page-home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import NotaFiscal from "@/components/nota-fiscal/nota-fiscal";
 
 export default function Home() {
   return (
-    <main className="grid grid-cols-2 gap-10 p-5 min-h-screen  font-[family-name:var(--font-geist-sans)]">
+    <main className="grid grid-cols-1 md:grid-cols-2 gap-10 p-5 min-h-screen  font-[family-name:var(--font-geist-sans)]">
       <FormNotaFiscal />
       <NotaFiscal />
     </main>
